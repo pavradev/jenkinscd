1a4af9d7e68856edd4ce4b1731b2b062d4960cba
pavel-r <rabeckijps@gmail.com>
2016-10-08 21:18:37 +0200

    Move dsl outside this project

--- seed.groovy
+++ seed.groovy
@@ -4,16 +4,16 @@ import hudson.tasks.Shell;
 import javaposse.jobdsl.plugin.*;
 import hudson.plugins.git.*;
 
-project = Jenkins.instance.createProject(FreeStyleProject, "seed")
+project = Jenkins.instance.createProject(FreeStyleProject, "myseed")
 
-gitSCM = new GitSCM("https://github.com/pavradev/jenkinscd.git")
+gitSCM = new GitSCM("https://github.com/pavradev/job-dsl-gradle-example.git")
 
 project.setScm(gitSCM)
 
 project.getBuildersList().clear()
 
 project.getBuildersList().add(new ExecuteDslScripts(
-  new ExecuteDslScripts.ScriptLocation("false","dsl/seed/*.groovy",null),
+  new ExecuteDslScripts.ScriptLocation("false","jobs/myseed.groovy",null),
   false,
   RemovedJobAction.DELETE,
   RemovedViewAction.DELETE,
